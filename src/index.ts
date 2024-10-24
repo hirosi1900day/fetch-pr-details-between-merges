8b2dd5c222caa206ed1c832b849dedf27c869d4b
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-10-24 21:13:32 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,67 @@
-/**
- * The entrypoint for the action.
- */
-import { run } from './main'
+import * as github from '@actions/github'
+import * as core from '@actions/core';
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-run()
+interface PRDetail {
+    pr_number: number;    // プルリクエストの番号
+    pr_title: string;     // プルリクエストのタイトル
+    pr_author: string;    // プルリクエストの作者
+    commit_sha: string;   // 関連するコミットのSHA
+}
+
+/**
+ * 指定された2つのマージコミット間のPR情報を抜き出します。
+ */
+async function getPRDetailsBetweenMerges() {
+    try {
+        const token: string = core.getInput('github-token')
+        const owner: string = core.getInput('repo-owner')
+        const repo: string = core.getInput('repo-name')
+        const base: string = core.getInput('from-sha')
+        const head: string = core.getInput('to-sha')
+
+        const octokit = github.getOctokit(token)
+        // 2つのSHA間のコミットを比較して取得
+        const { data: commits } = await octokit.rest.repos.compareCommits({
+            owner,
+            repo,
+            base,
+            head,
+          });
+
+        let prDetails: PRDetail[] = [];
+
+        // 各コミットについて処理
+        for (const commit of commits.commits) {
+            // マージコミットかどうかを確認
+            if (commit.parents && commit.parents.length > 1) {
+                // コミットに関連するPRを取得
+                const { data: pullRequests } = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+                    owner,
+                    repo,
+                    commit_sha: commit.sha
+                });
+
+                // 取得したPRの情報を抽出
+                pullRequests.forEach(pr  => {
+                    prDetails.push({
+                        pr_number: pr.number,
+                        pr_title: pr.title,
+                        pr_author: pr.user?.login || '',
+                        commit_sha: commit.sha
+                    });
+                });
+            }
+        }
+
+        // 出力を設定
+        core.setOutput("pr_details", JSON.stringify(prDetails));
+    } catch (error) {
+        // エラーが発生した場合は失敗を報告
+        if (error instanceof Error) {
+            core.setFailed(`Error fetching PR details: ${error.message}`);
+        }
+    }
+}
+
+// 環境変数からパラメータを取得して関数を実行
+getPRDetailsBetweenMerges();
