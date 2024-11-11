be42646501b073f9978a79fe221a45a428d028f2
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-11-11 22:59:42 +0900

    rebaseマージに対応する

--- dist/index.js
+++ dist/index.js
@@ -30060,40 +30060,39 @@ class PRDetailService {
         const seenPRNumbers = new Set();
         for (const commit of commits) {
             // スカッシュマージの場合、コミットメッセージにPR番号が含まれている
-            const prNumberMatch = commit.commit.message.match(/.+\s+\(#(\d+)\)/);
-            const isMergeCommit = commit.parents && commit.parents.length > 1;
-            if (isMergeCommit || prNumberMatch) {
-                const pullRequests = await this.gitHubClient.listPRsAssociatedWithCommit(commit.sha);
-                for (const pr of pullRequests.data) {
-                    if (!seenPRNumbers.has(pr.number)) {
-                        seenPRNumbers.add(pr.number);
-                        const prDetailsResponse = await this.gitHubClient.getPRDetail(pr.number);
-                        const sanitizedTitle = pr.title.replace(/["`]/g, '');
-                        // Conditionally build the PR detail object based on options
-                        const prDetail = {};
-                        if (options.includePRNumber) {
-                            prDetail.pr_number = pr.number;
-                        }
-                        if (options.includeCommitSha) {
-                            prDetail.commit_sha = commit.sha;
-                        }
-                        if (options.includeTitle) {
-                            prDetail.pr_title = sanitizedTitle;
-                        }
-                        if (options.includeAuthor) {
-                            prDetail.pr_author = pr.user?.login || '';
-                        }
-                        if (options.includeMergeUser) {
-                            prDetail.merge_user =
-                                prDetailsResponse.data.merged_by?.login || '';
-                        }
-                        if (options.includeMdLink) {
-                            prDetail.pr_md_link = `<${pr.html_url}|${sanitizedTitle}>`;
-                        }
-                        prDetails.push(prDetail);
+            // const prNumberMatch = commit.commit.message.match(/.+\s+\(#(\d+)\)/)
+            // const isMergeCommit = commit.parents && commit.parents.length > 1
+            // if (isMergeCommit || prNumberMatch) {
+            const pullRequests = await this.gitHubClient.listPRsAssociatedWithCommit(commit.sha);
+            for (const pr of pullRequests.data) {
+                if (!seenPRNumbers.has(pr.number)) {
+                    seenPRNumbers.add(pr.number);
+                    const prDetailsResponse = await this.gitHubClient.getPRDetail(pr.number);
+                    const sanitizedTitle = pr.title.replace(/["`]/g, '');
+                    // Conditionally build the PR detail object based on options
+                    const prDetail = {};
+                    if (options.includePRNumber) {
+                        prDetail.pr_number = pr.number;
                     }
+                    if (options.includeCommitSha) {
+                        prDetail.commit_sha = commit.sha;
+                    }
+                    if (options.includeTitle) {
+                        prDetail.pr_title = sanitizedTitle;
+                    }
+                    if (options.includeAuthor) {
+                        prDetail.pr_author = pr.user?.login || '';
+                    }
+                    if (options.includeMergeUser) {
+                        prDetail.merge_user = prDetailsResponse.data.merged_by?.login || '';
+                    }
+                    if (options.includeMdLink) {
+                        prDetail.pr_md_link = `<${pr.html_url}|${sanitizedTitle}>`;
+                    }
+                    prDetails.push(prDetail);
                 }
             }
+            // }
         }
         return { values: prDetails };
     }
